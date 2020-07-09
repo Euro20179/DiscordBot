b9e2ce94f5b029752f126f4a76512b49c05928af
Euro20179 <euro@Barney.localdomain>
2020-07-09 14:47:53 -0700

    5.0-rc10_2

--- common.py
+++ common.py
@@ -17,7 +17,7 @@ import youtube_dl
 from PIL import Image, ImageFilter, ImageEnhance, ImageOps, ImageDraw, ImageFont, ImageChops
 
 DELETE = "--delete"
-VERSION = "5.0-rc10"
+VERSION = "5.0-rc10_2"
 Stop = False
 
 playingGuessingGame = {}
