4745a6b8cf8cdd54f1e4699497b623b366456397
Euro20179 <euro@Barney.localdomain>
2020-07-21 09:50:08 -0700

    5.8.4.1

--- common.py
+++ common.py
@@ -20,7 +20,7 @@ from PIL import Image, ImageFilter, ImageEnhance, ImageOps, ImageDraw, ImageFont
 
 #TODO: emoteusage should type, and return txt file if the message is too long to send in chat
 
-VERSION = "5.8.4"
+VERSION = "5.8.4.1"
 Stop = False
 
 playingGuessingGame = {}
