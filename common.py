a8eea0ccda732dc25d2df6e373a24770040fd1f9
Euro20179 <euro@Barney.localdomain>
2020-08-06 10:39:51 -0700

    6.6.3

--- common.py
+++ common.py
@@ -22,7 +22,7 @@ from PIL import Image, ImageFilter, ImageEnhance, ImageOps, ImageDraw, ImageFont
 
 #TODO make each user an object with data relating to stuff like leveling and ping response, it will load in when they talk so it's not super slow at login time
 
-__version__ = "6.6.2"
+__version__ = "6.6.3"
 Stop = False
 
 playingHangman = {}
