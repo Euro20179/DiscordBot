259cb652ec1d366fe59ce448c094970a49685030
Euro20179 <idk>
2020-09-04 13:03:23 -0700

    7.9-rc1

--- common.py
+++ common.py
@@ -25,7 +25,7 @@ from PIL import Image, ImageFilter, ImageEnhance, ImageOps, ImageDraw, ImageFont
 
 #TODO userid: gets user id given a name
 #^ also channelid, emoteid, etc
-__version__ = "7.9"
+__version__ = "7.9-rc1"
 Stop = False
 
 playingHangman = {}
